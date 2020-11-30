c3f4a6e8b19a647b885c8f1323e7bd47cad8ce4c
Inbar Tamir <inbartamir92@gmail.com>
2020-11-30 22:16:42 +0200

    delete board

--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -2,7 +2,7 @@ const logger = require('../services/logger.service')
 
 async function requireAuth(req, res, next) {
   const board = req.body;
-  if (req.session && req.session.user && req.session.user === board.creator ) {
+  if (req.session && req.session.user && req.session.user !== board.creator ) {
     res.status(401).end('Unauthorized!');
     return;
   }
